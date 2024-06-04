1c73a26ef88866ae31b48922b7e10aa29b78aac7
origamiscienceguy <taylorsimsscience@gmail.com>
2024-06-04 00:22:15 -0500

    Displaying ships

--- source/scenes/gameplay.c
+++ source/scenes/gameplay.c
@@ -5,6 +5,7 @@ u16 tilemapBuffer0[1024] EWRAM_DATA;
 u16 tilemapBuffer1[1024] EWRAM_DATA;
 u16 tilemapBuffer2[1024] EWRAM_DATA;
 u16 tilemapBuffer3[1024] EWRAM_DATA;
+MapData mapState EWRAM_DATA;
 
 
 Scene gameplayScene = {
@@ -17,7 +18,7 @@ Scene gameplayScene = {
 };
 
 void gameplayInitialize(){
-	REG_DISPCNT = DCNT_MODE0 | DCNT_BG0 | /*DCNT_BG1 |*/ DCNT_OBJ;
+	REG_DISPCNT = DCNT_MODE0 | DCNT_BG0 | DCNT_BG1 | DCNT_OBJ;
 	REG_BG0CNT = BG_8BPP | BG_SBB(BG_0_TILEMAP) | BG_CBB(BG_0_CHARDATA);
 	REG_BG1CNT = BG_8BPP | BG_SBB(BG_1_TILEMAP) | BG_CBB(BG_0_CHARDATA);
 	
@@ -33,12 +34,12 @@ void gameplayInitialize(){
 	
 	//queue the tilemap for layer 0 to be sent
 	tilemapData[0].size = 512;
-	tilemapData[0].buffer = (void *)tilemapBuffer0;
+	tilemapData[0].buffer = tilemapBuffer0;
 	tilemapData[0].position = se_mem[BG_0_TILEMAP];
 	
 	//queue the tilemap for layer 1 to be sent
 	tilemapData[1].size = 512;
-	tilemapData[1].buffer = (void *)tilemapBuffer1;
+	tilemapData[1].buffer = tilemapBuffer1;
 	tilemapData[1].position = se_mem[BG_1_TILEMAP];
 	
 	//fill the tilemap buffer for layer 0
@@ -51,24 +52,143 @@ void gameplayInitialize(){
 		}
 	}
 	
-	//fill the tilemap buffer for layer 1
-	for(u32 i = 0; i < 16; i++){
-		for(u32 j = 0; j < 16; j++){
-			tilemapBuffer1[i * 64 + j * 2] = 1;
-			tilemapBuffer1[i * 64 + j * 2 + 1] = 1;
-			tilemapBuffer1[i * 64 + j * 2 + 32] = 1;
-			tilemapBuffer1[i * 64 + j * 2 + 33] = 1;
-		}
-	}
+	mapState.xPos = 0;
+	mapState.yPos = 0;
+	mapState.xLastPos = 0xffff;
+	mapState.yLastPos = 0xffff;
+	
+	initMap();
+	
+	createShipTilemap(shipList, tilemapBuffer1, &mapState);
 	
 	playNewAsset(_AreaA_DMA_Only);
 	currentScene.state = NORMAL;
-};
+}
 
 void gameplayNormal(){
 
-};
+}
 
 void gameplayEnd(){
 
-};
+}
+
+void createShipTilemap(ShipData *shipList, u16 *tilemapBuffer, MapData *mapState){
+	u8 xUpdateLow = 0; //lower bound of horizontal map cell that need to be updated
+	u8 xUpdateHigh = 0; //upper bound of horizontal map cell that need to be updated
+	u8 yUpdateLow = 0; //lower bound of vertical map cell that need to be updated
+	u8 yUpdateHigh = 0; //upper bound of vertical map cell that need to be updated
+	
+	//convert pixel coordinates to map cell coordinates
+	u8 mapXPos = mapState->xPos >> 4;
+	u8 mapYPos = mapState->yPos >> 4; 
+	u8 mapXLastPos = mapState->xLastPos >> 4;
+	u8 mapYLastPos = mapState->yLastPos >> 4;
+
+	//set the bound for x updates
+	if(mapXPos > mapXLastPos){
+		xUpdateLow = mapXLastPos + 16;
+		xUpdateHigh = mapXPos + 16;
+		
+		if((xUpdateLow + 16) < xUpdateHigh){
+			xUpdateLow = xUpdateHigh - 16;
+		}
+	}
+	else if(mapXPos < mapXLastPos){
+		xUpdateLow = mapXPos;
+		xUpdateHigh = mapXLastPos;
+		
+		if((xUpdateLow + 16) < xUpdateHigh){
+			xUpdateHigh = xUpdateLow + 16;
+		}
+	}
+	
+	//set the bounds for y updates
+	if(mapYPos > mapYLastPos){
+		yUpdateLow = mapYLastPos + 16;
+		yUpdateHigh = mapYPos + 16;
+		
+		if((yUpdateLow + 16) < yUpdateHigh){
+			yUpdateLow = yUpdateHigh - 16;
+		}
+	}
+	else if(mapYPos < mapYLastPos){
+		yUpdateLow = mapYPos;
+		yUpdateHigh = mapYLastPos;
+		
+		if((yUpdateLow + 16) < yUpdateHigh){
+			yUpdateHigh = yUpdateLow + 16;
+		}
+	}
+	
+	//clear the vertical column of newly arrived spaces
+	for(int i = xUpdateLow; i < xUpdateHigh; i++){
+		for(int j = 0; j < 16; j++){
+			tilemapBuffer[(i % 16) * 2 + (j % 16) * 64] = 3;
+			tilemapBuffer[(i % 16) * 2 + (j % 16) * 64 + 1] = 3;
+			tilemapBuffer[(i % 16) * 2 + (j % 16) * 64 + 32] = 3;
+			tilemapBuffer[(i % 16) * 2 + (j % 16) * 64 + 33] = 3;
+		}
+	}
+	
+	//draw every ship that should be on screen
+	for(u32 shipIndex = 0; shipIndex < mapState->numShips; shipIndex++){
+		u8 shipXPos = shipList[shipIndex].xPos;
+		u8 shipYPos = shipList[shipIndex].yPos;
+		
+		//if this particular ship is in one of the update regions
+		if(((shipXPos >= xUpdateLow) && (shipXPos < xUpdateHigh) && (shipYPos >= mapYPos) && (shipYPos <= mapYPos + 15))
+		|| ((shipYPos >= yUpdateLow) && (shipYPos < yUpdateHigh) && (shipXPos >= mapXPos) && (shipXPos <= mapYPos + 15))){
+			u16 baseIndex = (shipXPos % 16) * 2 + (shipYPos % 16) * 64;
+			u8 tilemapBase = (shipList[shipIndex].type + 1) * 4;
+			tilemapBuffer[baseIndex] = tilemapBase;
+			tilemapBuffer[baseIndex + 1] = tilemapBase + 1;
+			tilemapBuffer[baseIndex + 32] = tilemapBase + 2;
+			tilemapBuffer[baseIndex + 33] = tilemapBase + 3;
+		}
+	}
+	
+	//update the map previous position
+	mapState->xLastPos = mapState->xPos;
+	mapState->yLastPos = mapState->yPos;
+}
+
+//a temprary function to initialize a test map.
+void initMap(){
+	mapState.numShips = 5;
+	
+	shipList[0].type = RED_SCOUT;
+	shipList[0].state = READY;
+	shipList[0].index = 0;
+	shipList[0].health = 100;
+	shipList[0].xPos = 0;
+	shipList[0].yPos = 0;
+	
+	shipList[1].type = RED_BOMBER;
+	shipList[1].state = READY;
+	shipList[1].index = 1;
+	shipList[1].health = 100;
+	shipList[1].xPos = 5;
+	shipList[1].yPos = 5;
+	
+	shipList[2].type = RED_DESTROYER;
+	shipList[2].state = READY;
+	shipList[2].index = 2;
+	shipList[2].health = 100;
+	shipList[2].xPos = 10;
+	shipList[2].yPos = 10;
+	
+	shipList[3].type = RED_FIGHTER;
+	shipList[3].state = READY;
+	shipList[3].index = 3;
+	shipList[3].health = 100;
+	shipList[3].xPos = 5;
+	shipList[3].yPos = 10;
+	
+	shipList[4].type = RED_CRUISER;
+	shipList[4].state = READY;
+	shipList[4].index = 4;
+	shipList[4].health = 100;
+	shipList[4].xPos = 10;
+	shipList[4].yPos = 5;
+}
--- source/scenes/gameplay.h
+++ source/scenes/gameplay.h
@@ -15,31 +15,37 @@
 #define BG_1_TILEMAP 30
 
 //enums
-enum PlayerState{
-	PLAYER1, PLAYER2, PLAYER3, PLAYER4
-};
-
 enum ShipType{
-	SCOUT, FIGHTER, BOMBER, DESTROYER, CRUISER, BATTLESHIP, CARRIER
+	RED_SCOUT, RED_FIGHTER, RED_BOMBER, RED_DESTROYER, RED_CRUISER, RED_BATTLESHIP, RED_CARRIER,
+	BLUE_SCOUT, BLUE_FIGHTER, BLUE_BOMBER, BLUE_DESTROYER, BLUE_CRUISER, BLUE_BATTLESHIP, BLUE_CARRIER,
+	GREEN_SCOUT, GREEN_FIGHTER, GREEN_BOMBER, GREEN_DESTROYER, GREEN_CRUISER, GREEN_BATTLESHIP, GREEN_CARRIER,
+	YELLOW_SCOUT, YELLOW_FIGHTER, YELLOW_BOMBER, YELLOW_DESTROYER, YELLOW_CRUISER, YELLOW_BATTLESHIP, YELLOW_CARRIER,
 };
 
 enum ShipState{
 	READY, FINISHED, UNAVAILABLE, ACTION_0_PREMOVE, ACTION_1_PREMOVE, ACTION_2_PREMOVE,
-	ACTION_3_PREMOVE, ACTION_0_POSTMOVE, ACTION_1_POSTMOVE, ACTION_2_POSTMOVE, ACTION_3_POSTMOVE
+	ACTION_3_PREMOVE, ACTION_0_POSTMOVE, ACTION_1_POSTMOVE, ACTION_2_POSTMOVE, ACTION_3_POSTMOVE,
 };
 
 //structs
 typedef struct ShipData{
-	enum PlayerState player; //which player this ship belongs to
 	enum ShipType type; //the type of ship this one is
 	enum ShipState state; //the state of this ship
 	u8 index; //the index of this ship in the array of all ships
+	u8 health; //the health of this ship, 100 is max, 0 is dead
 	u8 xPos; //the x position of this ship on the map
 	u8 yPos; //the y position of this ship on the map
 	u8 xVel; //the x velocity of this ship
 	u8 yVel; //the y velocity of this ship
-	u8 health; //the health of this ship, 100 is max, 0 is dead
 }ShipData;
+
+typedef struct MapData{
+	u16 xPos; //the x position of the top left tile on screen
+	u16 yPos; //the y position of the top left tile on screen
+	u16 xLastPos; //the x position of the top left tile on screen last frame
+	u16 yLastPos; //the y position of the top left tile on screen last frame
+	u8 numShips; //the number of ships on the map.
+}MapData;
 
 //globals
 extern const unsigned int TestGfxPal[];
@@ -52,6 +58,10 @@ void gameplayNormal();
 void gameplayPause();
 void gameplayOutro();
 void gameplayEnd();
+void createShipTilemap(ShipData *, u16 *, MapData *);
+
+//temp function
+void initMap();
 
 //external functions
 
