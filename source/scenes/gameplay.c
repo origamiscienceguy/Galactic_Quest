57951131a1871c14df8421c77529c61eafb96831
origamiscienceguy <taylorsimsscience@gmail.com>
2024-06-08 04:12:59 -0500

    L and R now cycle ships!

--- source/scenes/gameplay.c
+++ source/scenes/gameplay.c
@@ -1,12 +1,11 @@
 #include "gameplay.h"
 
-ShipData shipList[MAX_SHIPS] EWRAM_DATA;
 u16 tilemapBuffer0[1024] EWRAM_DATA;
 u16 tilemapBuffer1[1024] EWRAM_DATA;
 u16 tilemapBuffer2[1024] EWRAM_DATA;
 u16 tilemapBuffer3[1024] EWRAM_DATA;
 u16 IOBuffer[30];
-MapData mapState EWRAM_DATA;
+MapData mapData EWRAM_DATA;
 
 
 Scene gameplayScene = {
@@ -53,95 +52,65 @@ void gameplayInitialize(){
 		}
 	}
 	
-	mapState.xPos = 0;
-	mapState.yPos = 0;
-	mapState.xLastPos = 0xffff;
-	mapState.yLastPos = 0xffff;
-	mapState.xSize = 255;
-	mapState.ySize = 255;
+	mapData.xPos = 0;
+	mapData.yPos = 0;
+	mapData.xLastPos = 0xffff;
+	mapData.yLastPos = 0xffff;
+	mapData.xSize = 255;
+	mapData.ySize = 255;
+	mapData.state = TURN_START;
+	mapData.teamTurn = RED_TEAM;
+	mapData.selectedShip = 0;
 	
+	//temporary function call to set up some ships like a saved scenareo would
 	initMap();
 	
-	createShipTilemap(shipList, tilemapBuffer1, &mapState);
+	//set up all of the linked lists of every ship
+	shipListInit();
+	
+	//generate the tilemap based on the camera position and the positions of all the ships
+	createShipTilemap(mapData.ships, tilemapBuffer1, &mapData);
 	
 	playNewAsset(_AreaA_DMA_Only);
 	currentScene.state = NORMAL;
 }
 
 void gameplayNormal(){
-
-	u16 scrollSpeed = 20;
-
-	//check inputs and move the map if needed
-	if(inputs.current & KEY_RIGHT){
-		if((mapState.xPos + scrollSpeed) > ((mapState.xSize << 4) - 223)){
-			mapState.xPos = (mapState.xSize << 4) - 223;
-		}
-		else{
-			mapState.xPos += scrollSpeed;
-		}
+	
+	switch(mapData.state){
+	case TURN_START:
+		turnStartState();
+		break;
+	case OPEN_MAP:
+		openMapState();
+		break;
+	case SHIP_SELECTED:
+		break;
+	case BATTLE:
+		break;
+	case TURN_END:
+		turnEndState();
+		break;
+	case TURN_REPLAY:
+		break;
 	}
-	
-	if(inputs.current & KEY_LEFT){
-		if((mapState.xPos - scrollSpeed) < 0){
-			mapState.xPos = 0;
-		}
-		else{
-			mapState.xPos -= scrollSpeed;
-		}
-	}
-	
-	if(inputs.current & KEY_DOWN){
-		if((mapState.yPos + scrollSpeed) > ((mapState.ySize << 4) - 143)){
-			mapState.yPos = (mapState.ySize << 4) - 143;
-		}
-		else{
-			mapState.yPos += scrollSpeed;
-		}
-	}
-	
-	if(inputs.current & KEY_UP){
-		if((mapState.yPos - scrollSpeed) < 0){
-			mapState.yPos = 0;
-		}
-		else{
-			mapState.yPos -= scrollSpeed;
-		}
-	}
-	
-	//queue the tilemap for layer 1 to be sent
-	tilemapData[1].size = 512;
-	tilemapData[1].buffer = tilemapBuffer1;
-	tilemapData[1].position = se_mem[BG_1_TILEMAP];
-	
-	//update the tilemap with the new position
-	createShipTilemap(shipList, tilemapBuffer1, &mapState);
-	
-	IOData[0].position = (void *)&REG_BG0HOFS;
-	IOData[0].buffer = IOBuffer;
-	IOData[0].size = 2;
-	
-	IOBuffer[0] = mapState.xPos % 512;
-	IOBuffer[1] = mapState.yPos % 512;
-	IOBuffer[2] = mapState.xPos % 512;
-	IOBuffer[3] = mapState.yPos % 512;
 }
 
 void gameplayEnd(){
 
 }
 
-void createShipTilemap(ShipData *shipList, u16 *tilemapBuffer, MapData *mapState){
+void createShipTilemap(ShipData *shipList, u16 *tilemapBuffer, MapData *mapData){
 	u16 xUpdateLow = 0; //lower bound of horizontal map cell that need to be updated
 	u16 xUpdateHigh = 0; //upper bound of horizontal map cell that need to be updated
 	u16 yUpdateLow = 0; //lower bound of vertical map cell that need to be updated
 	u16 yUpdateHigh = 0; //upper bound of vertical map cell that need to be updated
 	
 	//convert pixel coordinates to map cell coordinates
-	u8 mapXPos = mapState->xPos >> 4;
-	u8 mapYPos = mapState->yPos >> 4; 
-	u8 mapXLastPos = mapState->xLastPos >> 4;
-	u8 mapYLastPos = mapState->yLastPos >> 4;
+	u8 mapXPos = mapData->xPos >> 4;
+	u8 mapYPos = mapData->yPos >> 4; 
+	u8 mapXLastPos = mapData->xLastPos >> 4;
+	u8 mapYLastPos = mapData->yLastPos >> 4;
 
 	//set the bound for x updates
 	if(mapXPos > mapXLastPos){
@@ -200,7 +169,7 @@ void createShipTilemap(ShipData *shipList, u16 *tilemapBuffer, MapData *mapState
 	}
 	
 	//draw every ship that should be on screen
-	for(u32 shipIndex = 0; shipIndex < mapState->numShips; shipIndex++){
+	for(u32 shipIndex = 0; shipIndex < mapData->numShips; shipIndex++){
 		u8 shipXPos = shipList[shipIndex].xPos;
 		u8 shipYPos = shipList[shipIndex].yPos;
 		
@@ -218,53 +187,313 @@ void createShipTilemap(ShipData *shipList, u16 *tilemapBuffer, MapData *mapState
 	}
 	
 	//update the map previous position
-	mapState->xLastPos = mapState->xPos;
-	mapState->yLastPos = mapState->yPos;
+	mapData->xLastPos = mapData->xPos;
+	mapData->yLastPos = mapData->yPos;
+}
+
+void turnStartState(){
+	//create the active ship linked list based on which ships in the team linked list are still active
+	TeamData *teamPointer = &mapData.teams[mapData.teamTurn];
+	
+	//if this team has no active ships, end the turn immediately
+	if(teamPointer->state != ACTIVE){
+		mapData.state = TURN_END;
+		return;
+	}
+	
+	u32 firstShipIndex = teamPointer->firstShip;
+	u32 shipIndex = firstShipIndex;
+	
+	//find the first ship active on this team
+	while((mapData.ships[shipIndex].state == DESTROYED) || (mapData.ships[shipIndex].state == NOT_PARTICIPATING)){
+		shipIndex = mapData.ships[shipIndex].teamLink;
+		//if we run into the first ship index, it means all ships are dead
+		if(shipIndex == firstShipIndex){
+			teamPointer->state = DEFEATED;
+			mapData.state = TURN_END;
+			return;
+		}
+	}
+	
+	u32 numActiveShips = 0;
+	
+	//shipIndex now contains the first active ship on this team.
+	mapData.selectedShip = shipIndex;
+	u32 lastShipIndex = shipIndex;
+	u32 nextIndex = mapData.ships[shipIndex].teamLink;
+	//add all other active ships to the linked list
+	while(nextIndex != firstShipIndex){
+		if((mapData.ships[nextIndex].state != DESTROYED) && (mapData.ships[nextIndex].state != NOT_PARTICIPATING)){
+			mapData.ships[lastShipIndex].activeLink = nextIndex;
+			lastShipIndex = nextIndex;
+			numActiveShips++;
+		}
+		nextIndex = mapData.ships[nextIndex].teamLink;
+	}
+	
+	//finish up the loop with the last ship
+	mapData.ships[lastShipIndex].activeLink = shipIndex;
+	
+	teamPointer->numActiveShips = numActiveShips;
+	
+	mapData.state = OPEN_MAP;
+}
+
+void openMapState(){
+	u16 scrollSpeed = 20;
+	//check inputs and move the map if needed
+	if(inputs.current & KEY_RIGHT){
+		if((mapData.xPos + scrollSpeed) > ((mapData.xSize << 4) - 223)){
+			mapData.xPos = (mapData.xSize << 4) - 223;
+		}
+		else{
+			mapData.xPos += scrollSpeed;
+		}
+	}
+	if(inputs.current & KEY_LEFT){
+		if((mapData.xPos - scrollSpeed) < 0){
+			mapData.xPos = 0;
+		}
+		else{
+			mapData.xPos -= scrollSpeed;
+		}
+	}
+	if(inputs.current & KEY_DOWN){
+		if((mapData.yPos + scrollSpeed) > ((mapData.ySize << 4) - 143)){
+			mapData.yPos = (mapData.ySize << 4) - 143;
+		}
+		else{
+			mapData.yPos += scrollSpeed;
+		}
+	}
+	if(inputs.current & KEY_UP){
+		if((mapData.yPos - scrollSpeed) < 0){
+			mapData.yPos = 0;
+		}
+		else{
+			mapData.yPos -= scrollSpeed;
+		}
+	}
+	
+	//L and R cycle backwards or forwards through the active ships for this team, and center the camera on the next ship in the cycle
+	if(inputs.pressed & KEY_L){
+		//cycle through the linked list until we end up one before where we started
+		u32 currentIndex = mapData.selectedShip;
+		while(mapData.ships[currentIndex].activeLink != mapData.selectedShip){
+			currentIndex = mapData.ships[currentIndex].activeLink;
+		}
+		mapData.selectedShip = currentIndex;
+		
+		//check the bounds of the map
+		if(((mapData.ships[mapData.selectedShip].yPos << 4) - 72) < 0){
+			mapData.yPos = 0;
+		}
+		else if(((mapData.ships[mapData.selectedShip].yPos << 4) - 72) > ((mapData.ySize << 4) - 143)){
+			mapData.yPos = (mapData.ySize << 4) - 143;
+		}
+		else{
+			mapData.yPos = (mapData.ships[mapData.selectedShip].yPos << 4) - 72;
+		}
+		if(((mapData.ships[mapData.selectedShip].xPos << 4) - 112) < 0){
+			mapData.xPos = 0;
+		}
+		else if(((mapData.ships[mapData.selectedShip].xPos << 4) - 112) > ((mapData.xSize << 4) - 223)){
+			mapData.xPos = (mapData.xSize << 4) - 223;
+		}
+		else{
+			mapData.xPos = (mapData.ships[mapData.selectedShip].xPos << 4) - 112;
+		}
+	}
+	if(inputs.pressed & KEY_R){
+		mapData.selectedShip = mapData.ships[mapData.selectedShip].activeLink;
+		//check the bounds of the map
+		if(((mapData.ships[mapData.selectedShip].yPos << 4) - 72) < 0){
+			mapData.yPos = 0;
+		}
+		else if(((mapData.ships[mapData.selectedShip].yPos << 4) - 72) > ((mapData.ySize << 4) - 143)){
+			mapData.yPos = (mapData.ySize << 4) - 143;
+		}
+		else{
+			mapData.yPos = (mapData.ships[mapData.selectedShip].yPos << 4) - 72;
+		}
+		if(((mapData.ships[mapData.selectedShip].xPos << 4) - 112) < 0){
+			mapData.xPos = 0;
+		}
+		else if(((mapData.ships[mapData.selectedShip].xPos << 4) - 112) > ((mapData.xSize << 4) - 223)){
+			mapData.xPos = (mapData.xSize << 4) - 223;
+		}
+		else{
+			mapData.xPos = (mapData.ships[mapData.selectedShip].xPos << 4) - 112;
+		}
+	}
+	
+	//queue the tilemap for layer 1 to be sent
+	tilemapData[1].size = 512;
+	tilemapData[1].buffer = tilemapBuffer1;
+	tilemapData[1].position = se_mem[BG_1_TILEMAP];
+	
+	//update the tilemap with the new position
+	createShipTilemap(mapData.ships, tilemapBuffer1, &mapData);
+	
+	IOData[0].position = (void *)&REG_BG0HOFS;
+	IOData[0].buffer = IOBuffer;
+	IOData[0].size = 2;
+	
+	IOBuffer[0] = mapData.xPos % 512;
+	IOBuffer[1] = mapData.yPos % 512;
+	IOBuffer[2] = mapData.xPos % 512;
+	IOBuffer[3] = mapData.yPos % 512;
+}
+
+//after this player has selected "end turn"
+void turnEndState(){
+	
+}
+
+//initialize all of the linked lists of the ships in this scenario
+void shipListInit(){
+	//initialize all four teams
+	for(u32 teamIndex = 0; teamIndex < NUM_TEAMS; teamIndex++){
+		TeamData *teamPointer = &mapData.teams[teamIndex];
+		teamPointer->state = ABSENT;
+		teamPointer->numStartingShips = 0;
+	}
+	
+	u32 lastShipIndex[NUM_TEAMS]; //the last found ship for this particular team
+	
+	//check every ship
+	for(u32 shipIndex = 0; shipIndex < MAX_SHIPS; shipIndex++){
+		ShipData *shipPointer = &mapData.ships[shipIndex];
+		u32 shipTeam = shipPointer->team;
+		TeamData *teamPointer = &mapData.teams[shipTeam];
+		
+		//all active ships must be initialized with the ready state
+		if(shipPointer->state != READY){
+			continue;
+		}
+		
+		//increment the number of ships for the team this ship belongs to
+		teamPointer->numStartingShips++;
+		//if this was the first ship for a particular team, activate that team
+		if(teamPointer->state == ABSENT){
+			teamPointer->state = ACTIVE;
+			teamPointer->firstShip = shipIndex;
+		}
+		//if this team has other ships as well, link this ship to the previous to extend the linked list
+		else{
+			mapData.ships[lastShipIndex[shipTeam]].teamLink = shipIndex;
+		}
+		lastShipIndex[shipTeam] = shipIndex;
+		
+		mapData.ships[shipIndex].sameTileLink = shipIndex;
+		
+		//check if any of the other ships before this one have the same exact position on the map
+		u32 xPos = mapData.ships[shipIndex].xPos;
+		u32 yPos = mapData.ships[shipIndex].yPos;
+		for(s32 overlapShipIndex = shipIndex - 1; overlapShipIndex >= 0; overlapShipIndex--){
+			if((mapData.ships[overlapShipIndex].xPos == xPos) && (mapData.ships[overlapShipIndex].yPos == yPos)){
+				mapData.ships[overlapShipIndex].sameTileLink = shipIndex;
+				break;
+			}
+		}
+	}
+	
+	//complete the linked list loop for each team that has at least one ship
+	for(u32 teamIndex = 0; teamIndex < NUM_TEAMS; teamIndex++){
+		TeamData *teamPointer = &mapData.teams[teamIndex];
+		if(teamPointer->state == ABSENT){
+			continue;
+		}
+		
+		mapData.ships[lastShipIndex[teamIndex]].teamLink = teamPointer->firstShip;
+	}
+	
+	//complete the linked list loops for every group of ships that share a tile
+	for(u32 shipIndex = 0; shipIndex < MAX_SHIPS; shipIndex++){
+		//if it points to itself, this ship does not share a tile with any others
+		if(mapData.ships[shipIndex].sameTileLink == shipIndex){
+			continue;
+		}
+		//if this ship is not active, skip it.
+		if(mapData.ships[shipIndex].state != READY){
+			continue;
+		}
+		
+		u32 nextIndex = mapData.ships[shipIndex].sameTileLink;
+		//otherwise, follow this linked list until we reach the end, and then connect the end to this front.
+		while(mapData.ships[nextIndex].sameTileLink != shipIndex){
+			//if we find a ship on this chain that points to itself, it is the last ship in the chain
+			if(mapData.ships[nextIndex].sameTileLink == nextIndex){
+				mapData.ships[nextIndex].sameTileLink = shipIndex;
+				break;
+			}
+			nextIndex = mapData.ships[nextIndex].sameTileLink;
+		}
+	}
 }
 
 //a temprary function to initialize a test map.
 void initMap(){
-	mapState.numShips = 6;
+	mapData.numShips = 6;
+	mapData.teams[BLUE_TEAM].state = ABSENT;
+	mapData.teams[GREEN_TEAM].state = ABSENT;
+	mapData.teams[YELLOW_TEAM].state = ABSENT;
 	
-	shipList[0].type = RED_SCOUT;
-	shipList[0].state = READY;
-	shipList[0].index = 0;
-	shipList[0].health = 100;
-	shipList[0].xPos = 0;
-	shipList[0].yPos = 0;
+	mapData.ships[0].type = SCOUT;
+	mapData.ships[0].state = READY;
+	mapData.ships[0].team = RED_TEAM;
+	mapData.ships[0].health = 100;
+	mapData.ships[0].xPos = 0;
+	mapData.ships[0].yPos = 0;
+	mapData.ships[0].xVel = 5;
+	mapData.ships[0].yVel = 5;
 	
-	shipList[1].type = RED_BOMBER;
-	shipList[1].state = READY;
-	shipList[1].index = 1;
-	shipList[1].health = 100;
-	shipList[1].xPos = 16;
-	shipList[1].yPos = 16;
+	mapData.ships[1].type = BOMBER;
+	mapData.ships[1].state = READY;
+	mapData.ships[1].team = RED_TEAM;
+	mapData.ships[1].health = 100;
+	mapData.ships[1].xPos = 16;
+	mapData.ships[1].yPos = 16;
+	mapData.ships[1].xVel = -1;
+	mapData.ships[1].yVel = 1;
 	
-	shipList[2].type = RED_DESTROYER;
-	shipList[2].state = READY;
-	shipList[2].index = 2;
-	shipList[2].health = 100;
-	shipList[2].xPos = 17;
-	shipList[2].yPos = 5;
+	mapData.ships[2].type = DESTROYER;
+	mapData.ships[2].state = READY;
+	mapData.ships[2].team = RED_TEAM;
+	mapData.ships[2].health = 100;
+	mapData.ships[2].xPos = 17;
+	mapData.ships[2].yPos = 5;
+	mapData.ships[2].xVel = -3;
+	mapData.ships[2].yVel = 1;
 	
-	shipList[3].type = RED_FIGHTER;
-	shipList[3].state = READY;
-	shipList[3].index = 3;
-	shipList[3].health = 100;
-	shipList[3].xPos = 5;
-	shipList[3].yPos = 17;
+	mapData.ships[3].type = FIGHTER;
+	mapData.ships[3].state = READY;
+	mapData.ships[3].team = RED_TEAM;
+	mapData.ships[3].health = 100;
+	mapData.ships[3].xPos = 5;
+	mapData.ships[3].yPos = 17;
+	mapData.ships[3].xVel = 4;
+	mapData.ships[3].yVel = 10;
 	
-	shipList[4].type = RED_CRUISER;
-	shipList[4].state = READY;
-	shipList[4].index = 4;
-	shipList[4].health = 100;
-	shipList[4].xPos = 10;
-	shipList[4].yPos = 5;
+	mapData.ships[4].type = CRUISER;
+	mapData.ships[4].state = READY;
+	mapData.ships[4].team = RED_TEAM;
+	mapData.ships[4].health = 100;
+	mapData.ships[4].xPos = 10;
+	mapData.ships[4].yPos = 5;
+	mapData.ships[4].xVel = 16;
+	mapData.ships[4].yVel = 16;
 	
-	shipList[5].type = RED_BATTLESHIP;
-	shipList[5].state = READY;
-	shipList[5].index = 5;
-	shipList[5].health = 100;
-	shipList[5].xPos = 255;
-	shipList[5].yPos = 255;
+	mapData.ships[5].type = BATTLESHIP;
+	mapData.ships[5].state = READY;
+	mapData.ships[5].team = RED_TEAM;
+	mapData.ships[5].health = 100;
+	mapData.ships[5].xPos = 255;
+	mapData.ships[5].yPos = 255;
+	mapData.ships[5].xVel = -50;
+	mapData.ships[5].yVel = -50;
+	
+	for(u32 i = mapData.numShips; i < MAX_SHIPS; i++){
+		mapData.ships[i].state = NOT_PARTICIPATING;
+	}
 }
--- source/scenes/gameplay.h
+++ source/scenes/gameplay.h
@@ -8,6 +8,7 @@
 //constants
 #define MAX_SHIPS 256
 #define MAX_MAP_SIZE 256
+#define NUM_TEAMS 4
 
 #define BG_0_TILEMAP 31
 #define BG_0_CHARDATA 0
@@ -16,30 +17,50 @@
 
 //enums
 enum ShipType{
-	RED_SCOUT, RED_FIGHTER, RED_BOMBER, RED_DESTROYER, RED_CRUISER, RED_BATTLESHIP, RED_CARRIER,
-	BLUE_SCOUT, BLUE_FIGHTER, BLUE_BOMBER, BLUE_DESTROYER, BLUE_CRUISER, BLUE_BATTLESHIP, BLUE_CARRIER,
-	GREEN_SCOUT, GREEN_FIGHTER, GREEN_BOMBER, GREEN_DESTROYER, GREEN_CRUISER, GREEN_BATTLESHIP, GREEN_CARRIER,
-	YELLOW_SCOUT, YELLOW_FIGHTER, YELLOW_BOMBER, YELLOW_DESTROYER, YELLOW_CRUISER, YELLOW_BATTLESHIP, YELLOW_CARRIER,
+	SCOUT, FIGHTER, BOMBER, DESTROYER, CRUISER, BATTLESHIP, CARRIER,
 };
 
 enum ShipState{
-	READY, FINISHED, UNAVAILABLE, ACTION_0_PREMOVE, ACTION_1_PREMOVE, ACTION_2_PREMOVE,
-	ACTION_3_PREMOVE, ACTION_0_POSTMOVE, ACTION_1_POSTMOVE, ACTION_2_POSTMOVE, ACTION_3_POSTMOVE,
+	READY, FINISHED, UNAVAILABLE, DESTROYED, NOT_PARTICIPATING
+};
+
+enum MapState{
+	TURN_START, OPEN_MAP, SHIP_SELECTED, BATTLE, TURN_END, TURN_REPLAY
+};
+
+enum TeamState{
+	ABSENT, ACTIVE, DEFEATED
+};
+
+enum Team{
+	RED_TEAM, BLUE_TEAM, GREEN_TEAM, YELLOW_TEAM
 };
 
 //structs
 typedef struct ShipData{
 	enum ShipType type; //the type of ship this one is
 	enum ShipState state; //the state of this ship
-	u8 index; //the index of this ship in the array of all ships
+	enum Team team; //which team this ship belongs to
 	u8 health; //the health of this ship, 100 is max, 0 is dead
 	u8 xPos; //the x position of this ship on the map
 	u8 yPos; //the y position of this ship on the map
 	u8 xVel; //the x velocity of this ship
 	u8 yVel; //the y velocity of this ship
+	u8 teamLink; //the index of the next ship of the same team, forming a linked list.
+	u8 activeLink; //the index of the next ship that has not yet moved, forming a linked list.
+	u8 sameTileLink; //the index of the next ship in the same tile as this one, forming a linked list. If this points to itself, there are no others
 }ShipData;
 
+typedef struct TeamData{
+	enum TeamState state; //the state of this team in this particular battle
+	u8 firstShip; //the index of the first ship in every team a new turn will begin with this ship highlighted
+	u8 numStartingShips; //the number of ships each team began the battle with
+	u8 numActiveShips; //the number of ships each team has that is currently alive
+}TeamData;
+
 typedef struct MapData{
+	enum MapState state; //what is the stage of the game are we in
+	enum Team teamTurn; //who's turn is it
 	u16 xPos; //the x position of the top left tile on screen
 	u16 yPos; //the y position of the top left tile on screen
 	u16 xLastPos; //the x position of the top left tile on screen last frame
@@ -47,6 +68,9 @@ typedef struct MapData{
 	u8 xSize; //the number of tiles large the map is in the x direction
 	u8 ySize; //number of tiles in the y direction
 	u8 numShips; //the number of ships on the map.
+	u8 selectedShip; //the index of the ship currently/last selected
+	ShipData ships[MAX_SHIPS]; //the array of information about every ship in this battle
+	TeamData teams[NUM_TEAMS]; //the array of information about each team in the battle
 }MapData;
 
 //globals
@@ -60,7 +84,11 @@ void gameplayNormal();
 void gameplayPause();
 void gameplayOutro();
 void gameplayEnd();
+void shipListInit();
 void createShipTilemap(ShipData *, u16 *, MapData *);
+void turnStartState();
+void openMapState();
+void turnEndState();
 
 //temp function
 void initMap();
