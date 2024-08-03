653999177256e91db97e64816ef59b578bd92d28
potatoTeto <potato.cos@gmail.com>
2024-08-03 14:21:23 -0700

    refactor: Audio folder structure and filename refactor; conv_audio.py script updated to reflect the changes

--- data/audio/assets_priorities/_musThemeANewIdea_Priority.c
+++ data/audio/assets_priorities/_musThemeANewIdea_Priority.c
@@ -2,7 +2,7 @@
 #include "tonc.h"
 
 //type in the priority of this asset. Higher number means higher priority
-cu8 _ThemeC_Battle_Priority = 100;
+cu8 _musThemeANewIdea_Priority = 0;
 
 //type in the priority of each channel in this asset. Leave unused channels as blank, or set to 0
-cu8 _ThemeC_Battle_ChannelPriority[MAX_DMA_CHANNELS] = {100,100,100,100,0,0,0,0,0,0};
+cu8 _musThemeANewIdea_ChannelPriority[MAX_DMA_CHANNELS] = {100, 100, 100, 100, 100, 100, 100, 100, 100, 100};
--- data/audio/assets_priorities/_musThemeC_Priority.c
+++ data/audio/assets_priorities/_musThemeC_Priority.c
@@ -2,7 +2,7 @@
 #include "tonc.h"
 
 //type in the priority of this asset. Higher number means higher priority
-cu8 _ThemeC_Priority = 100;
+cu8 _musThemeC_Priority = 100;
 
 //type in the priority of each channel in this asset. Leave unused channels as blank, or set to 0
-cu8 _ThemeC_ChannelPriority[MAX_DMA_CHANNELS] = {100, 100, 100, 100, 100, 100, 100, 100, 100, 100};
+cu8 _musThemeC_ChannelPriority[MAX_DMA_CHANNELS] = {100, 100, 100, 100, 100, 100, 100, 100, 100, 100};
