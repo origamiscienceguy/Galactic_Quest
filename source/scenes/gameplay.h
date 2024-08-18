47f061445bb04893a501f2b67d57d91d067a6d67
potatoTeto <potato.cos@gmail.com>
2024-08-18 14:19:32 -0700

    Update gameplay.h

--- source/scenes/gameplay.h
+++ source/scenes/gameplay.h
@@ -176,13 +176,6 @@ enum BattleMenuState{
 	BATTLE_ON, BATTLE_OFF
 };
 
-enum AUDGROUP_IDS{
-    AUDGROUP_MENUSFX,
-    AUDGROUP_SOUNDT_SFX,
-    AUDGROUP_SOUNDT_BGM,
-	AUDGROUP_MAX
-};
-
 //structs
 typedef struct ShipData{
 	enum ShipType type; //the type of ship this one is
